3339e520988e004408715a8349b93f829bfb0601
Millü <jssmiller94@gmail.com>
2025-03-04 11:03:42 +0000

    learnt that copy() function is needed to create copy_last_row, otherwise it points to same reference location as self.road_grid and updates every list

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -11,14 +12,14 @@ class Traffic:
         #initialise 1D grid of "cars" using the given parameters 
         #append new iterations to grid, starting with initial 1D grid
         # grid has empty zeros at each end- halo region- in this example 
-        
         # self.road_length is 6
         self.road_grid = [[0, 0, 0, 1, 0, 1, 1, 0]]
+        
         # copy of list with halo looks like [1, 0, 0, 1, 0, 1, 1, 0]
 
     def __str__(self):
         s = ""
-        for row in self.grid:
+        for row in self.road_grid:
             for point in row:
                 s+= f"{point}"
             s += "\n"
@@ -30,30 +31,35 @@ class Traffic:
 
     def move_car(self):
 
-        #first find out if there are cars at either end of the road
-        #and copy them to a new list
-        
-        # list that user sees [0, 0, 1, 0, 1, 1]
-        # init list     =  [0, 0, 0, 1, 0, 1, 1, 0]
-        # function inside init determines the new halo list for each iteration
-        # halo list     =  [1, 0, 0, 1, 0, 1, 1, 0]
-        # halo list is ready to be updated with move_car()
-        # new list      =  [0, 1, 0, 0, 1, 1, 0, 0]
+        """
+        first find out if there are cars at either end of the road
+        and copy them to a new list
+        list that user sees [0, 0, 1, 0, 1, 1]
+        init list     =  [0, 0, 0, 1, 0, 1, 1, 0]
+        function inside init determines the new halo list for each iteration
+        halo list     =  [1, 0, 0, 1, 0, 1, 1, 0]
+        halo list is ready to be updated with move_car()
+        new list      =  [0, 1, 0, 0, 1, 1, 0, 0]
+        """     
 
         # copy last row of self.road grid and modify to include updated halo
-        copy_last_row = self.road_grid[-1]
+        copy_last_row = self.road_grid[-1].copy()
+
+        print(f"{copy_last_row} copy_last_row")
 
         # checks LH inner cell to update RH halo
-        if copy_last_row[1] == 0:
+        if copy_last_row[1] == 1:
             copy_last_row[-1] = 1
+        print(f"{copy_last_row} LH inner update RH cell")
         # checks RH inner cell to update LH halo
         if copy_last_row[-2] == 1:
             copy_last_row[0] = 1
+        print(f"{copy_last_row} RH inner update LH cell")
         
+        print(f"{self.road_grid} self.road_grid")
 
-        """
         
-        for index, space in self.road_grid:
+        for index, space in enumerate(copy_last_row):
 
             # skips the halo region
             if index == 0 or -1:
@@ -65,12 +71,11 @@ class Traffic:
                     space = 1
                 else:
                     space = 0
+        
+        
 
-        """
-
-
-        self.road_grid.append([0, 0, 0, 1, 0, 1, 1, 0])
-        print(self.road_grid)
+        self.road_grid.append(copy_last_row)
+        # print(self.road_grid)
 
 def main():
 
@@ -79,6 +84,7 @@ def main():
     car_density = 0.5
 
     traffic_model = Traffic(number_of_cells, number_of_iterations, car_density)
+    print(traffic_model)
 
     traffic_model.move_car()
     print(traffic_model)
