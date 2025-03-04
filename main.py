c19b9421285ae0862133594cb51178bcc2b83d8f
Millü <jssmiller94@gmail.com>
2025-03-04 12:55:36 +0000

    move_cars_one_step() behaves as expected

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ class Traffic:
         #append new iterations to grid, starting with initial 1D grid
         # grid has empty zeros at each end- halo region- in this example 
         # self.road_length is 6
-        self.road_grid = [[0, 0, 0, 1, 0, 1, 1, 0]]
+        self.road_grid = [[0, 0, 1, 0, 1, 1]]
+        #self.road_grid = [[0, 0, 0, 1, 0, 1, 1, 0]]
+        #self.road_grid = [[0, 1, 0, 1, 0, 1, 1, 0]]
+        #self.road_grid = [[0, 0, 0, 1, 0, 1, 1, 0]]
         
         # copy of list with halo looks like [1, 0, 0, 1, 0, 1, 1, 0]
 
@@ -29,7 +32,7 @@ class Traffic:
     function to move cars forwards if space is free
     """
 
-    def move_car(self):
+    def move_cars_one_step(self):
 
         """
         first find out if there are cars at either end of the road
@@ -47,6 +50,12 @@ class Traffic:
 
         print(f"{copy_last_row} copy_last_row")
 
+        #adds zeros to both ends
+        copy_last_row.insert(0,0)
+        copy_last_row.append(0)
+
+        print(f"{copy_last_row} copy_last_row")
+
         # checks LH inner cell to update RH halo
         if copy_last_row[1] == 1:
             copy_last_row[-1] = 1
@@ -58,23 +67,42 @@ class Traffic:
         
         print(f"{self.road_grid} self.road_grid")
 
+        print(f"{copy_last_row} copy_last_row updated halo")
+
+        updated_road = [0] * len(copy_last_row)
+        print(f"{updated_road} updated road")
         
         for index, space in enumerate(copy_last_row):
 
-            # skips the halo region
-            if index == 0 or -1:
-                continue
+            # ignore last item in array (RH halo)
+            if index == len(copy_last_row) -1:
+                break
 
+            #print(f"{index}")
+
+            # print(f"{copy_last_row} copy_last_row loop {index}")
             if space == 1:
                 #if the space in front is occupied then remain stationary
-                if self.road_grid[index + 1] == 1:
-                    space = 1
+                if copy_last_row[index + 1] == 1:
+                    updated_road[index] = 1
                 else:
-                    space = 0
+                    updated_road[index] = 0
+            else:
+                if copy_last_row[index -1] == 1:
+                    updated_road[index] = 1
+                else:
+                    updated_road == 0
+            #print(f"{updated_road} updated road {index}")
+
+        #print(f"{updated_road} updated road")
         
+        # remove values from either end of updated list
+        updated_road.pop(0)
+        updated_road.pop(-1)
         
 
-        self.road_grid.append(copy_last_row)
+        # self.road_grid.append(copy_last_row)
+        self.road_grid.append(updated_road)
         # print(self.road_grid)
 
 def main():
@@ -86,7 +114,7 @@ def main():
     traffic_model = Traffic(number_of_cells, number_of_iterations, car_density)
     print(traffic_model)
 
-    traffic_model.move_car()
+    traffic_model.move_cars_one_step()
     print(traffic_model)
 
 if __name__ == "__main__":
